cc5728e75f1232bc25ca88ade6f3181ed8d212dc
Ziyao Cheng <yottacheng3@gmail.com>
2023-12-07 23:40:21 +0000

    new

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+
+        key = str(item)
+
+        if key in frequencies:
+            frequencies[key] += 1
+        else:
+
+            frequencies[key] = 1
     return frequencies
